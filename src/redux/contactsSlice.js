33a2f9610a0e56637f5cbec8e0d913767c26e0a6
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-09-25 17:20:15 +1000

    Create contactsSlice.js

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsInitialState = [];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            text,
+            id: nanoid(),
+            completed: false,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      const index = state.findIndex(task => task.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
