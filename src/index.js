8934f9f8268930aed3298d30694d215e858c32bb
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-10-05 17:29:34 +1100

    primera configuracion de libreria redux-persist

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //redux imports
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 //app imports
 import { App } from 'components/App';
 import './index.css';
@@ -10,7 +11,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,21 @@
-//import { createStore } from "redux";
-//import { devToolsEnhancer } from "@redux-devtools/extension";
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer  } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const myPersistReducer = persistReducer(persistConfig, contactsReducer)
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: myPersistReducer,
     filter: filterReducer,
   }
 });
 
-
-
-/* const rootReducer = (state = initialState, action) => {
-  return state;
-};
-
-// Crear una extensión del store para añadir las herramientas de desarrollador.
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);*/
+export const persistor = persistStore(store);
