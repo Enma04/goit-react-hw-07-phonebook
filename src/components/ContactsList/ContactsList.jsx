09961166e892a9e5909667881f10ebbe79577c03
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-10-08 15:20:38 +1100

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -5,24 +5,27 @@ import { deleteContact } from "redux/contactsSlice";
 import Swal from "sweetalert2";
 
 export const ContactsList = () => {
+  //-----------------------------------------
+  //--------------- VARIABLES
   let filtro = useSelector(state => state.filter.value);
   let list = useSelector(state => state.contacts.list);
-  let contacts = [];
+  let contacts = list;
   let dispatch = useDispatch();
 
   if( filtro !== '' ) {
     contacts = list.filter(item => item.name.toLowerCase()
                    .includes(filtro.toLowerCase()));
   }
-  else {
-    contacts = list;
-  }
 
+  //-----------------------------------------
+  //--------------- METHODS
   const onDelete = (id, name) => {
     dispatch( deleteContact(id) );
     Swal.fire(`${name} eliminado!`);
   }
 
+  //-----------------------------------------
+  //--------------- COMPONENT
   return(
     <ul className={css.contactList}>
       {contacts.length !== 0 &&
